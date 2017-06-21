275cc79402406c5d8fca934885b129121ea9bd23
zhangtuzi <1243933019@qq.com>
2017-06-21 17:24:06 +0800

    '拖拽出现新问题，父级接收模板后，子级无法接收模板'

--- js/backIndex2.js
+++ js/backIndex2.js
@@ -47,59 +47,6 @@ $('.AdStyle>li').click(function(){
   $(this).addClass('AdLiSele');
 })
 
-// 模板模块拖动
-//拖拽复制体
-$('.mubanMove').draggable({
- helper:"clone",
- cursor: "move",
-});
-
-var Bg1='<div class="moveBg"><img src="images/backStage/bg_bk1.png" class="bg_bk1">';
-  Bg1+='<img src="images/backStage/bg_bk2.png" class="bg_bk2">';
-  Bg1+='<img src="images/backStage/bg_bk3.png" class="bg_bk3">';
-  Bg1+='<img src="images/backStage/bg_bk4.png" class="bg_bk4">';
-  Bg1+='</div><div class="moveTip1">松开鼠标，模块会放到这里</div>';
-
-var Bg2='<div class="moveBg"><img src="images/backStage/bg_bk1.png" class="bg_bk1">';
-  Bg2+='<img src="images/backStage/bg_bk2.png" class="bg_bk2">';
-  Bg2+='<img src="images/backStage/bg_bk3.png" class="bg_bk3">';
-  Bg2+='<img src="images/backStage/bg_bk4.png" class="bg_bk4">';
-  Bg2+='</div><div class="moveTip2">此处不支持该模板</div>';
-
-
-//拖拽放置
-var moveFlag=true;//定义是否可以放置变量
-$('.droppable').droppable({
-  hoverClass: 'moveArea',//拖拽模块移入放置区域时添加样式
-  greedy:true,//禁止父级接收拖拽目标
-  //拖拽模块移入放置区域时选择提示语
-  over:function(event,ui){
-    var source = ui.draggable.clone();
-    if($(this).hasClass('rqtjMove')){//如果放置区域含有标识，则可以提示可以放置
-      $(this).append(Bg1);
-      moveFlag=true;
-    }else{
-     $(this).append(Bg2);
-     moveFlag=false;
-    }
-  },
-  //拖拽模块移出放置区域时清空提示语
-  out:function(){
-    $('.moveBg,.moveTip1,.moveTip2').remove();
-  },
-  //拖拽模块在放置区域放置时
-  drop:function(event,ui){
-    //如果放置变量为true，则将放置区域清空，将拖拽模块放入
-    if(moveFlag){
-      $(this).children().remove();
-       //  var source = ui.draggable.next().clone().css('display','block');
-       var source = ui.draggable.clone();
-        $(this).append(source);
-    }else{//不能放置则将提示语清空
-      $('.moveBg,.moveTip1,.moveTip2').remove();
-    }
- }
-})
 
 //对于放置区域里面的元素进行代理
 $('.droppable').delegate('','mouseover',function(e){
@@ -191,22 +138,7 @@ $('.droppable').delegate('img','dblclick',function(e){
 //联通精选部分切换按钮
 $('.lx_jx_sx_ul>li').click(function(){
   var this_index=$(this).index();
-  switch(this_index){
-    case 0 :
-    $('.jx_rqtjLi').show();
-    $('.jx_cnxhLi,.jx_tszqLi,.jx_xsgLi').hide();
-    break;
-    case 1 :
-    $('.jx_tszqLi').show();
-    $('.jx_rqtjLi,.jx_cnxhLi,.jx_xsgLi').hide();
-    break;
-    case 2 :
-    $('.jx_cnxhLi').show();
-    $('.jx_rqtjLi,.jx_tszqLi,.jx_xsgLi').hide();
-    break;
-    default:
-    $('.jx_xsgLi').show();
-    $('.jx_rqtjLi,.jx_tszqLi,.jx_cnxhLi').hide();
-  }
+  $('.lx_jx_pro_ul').hide();
+  $('.lx_jx_pro_ul').eq(this_index).show();
   $(this).addClass('lx_jx_sx_select').siblings().removeClass('lx_jx_sx_select');
 })
